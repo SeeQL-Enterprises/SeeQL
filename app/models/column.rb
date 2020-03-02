6bc88b5ba202232e2b669bde846724d11efd5b73
Finn Rub <r.finn40@yahoo.com>
2020-03-02 16:19:30 +0100

    added all postgres data types

--- app/models/column.rb
+++ app/models/column.rb
@@ -1,3 +1,10 @@
 class Column < ApplicationRecord
   belongs_to :table
+  enum data_type: ["bigint", "bigserial", "bit [ (n) ]", "bit varying [ (n) ]", "boolean",
+                    "box", "bytea", "character [ (n) ]", "character varying [ (n) ]", "cidr", "circle",
+                    "date", "double precision", "inet", "integer", "interval [ fields ] [ (p) ]",
+                    "json", "jsonb", "line", "lseg", "macaddr", "money", "numeric [ (p, s) ]", "path",
+                    "pg_lsn", "point", "polygon", "real", "smallint", "smallserial", "serial",
+                    "text", "time [ (p) ] [ without time zone ]", "time [ (p) ] with time zone"; "timestamp [ (p) ] [ without time zone ]",
+                    "timestamp [ (p) ] with time zone", "tsquery", "tsvector", "txid_snapshot", "uuid", "xml"]
 end
