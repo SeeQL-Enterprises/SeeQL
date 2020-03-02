21b7252695c68693d2d377b7d1a9821a48054ab8
Tim Vierboom <tim-vierboom@gmx.de>
2020-03-02 18:37:15 +0100

    created a comment model

---
+++ db/migrate/20200302173651_create_comments.rb
@@ -0,0 +1,12 @@
+class CreateComments < ActiveRecord::Migration[5.2]
+  def change
+    create_table :comments do |t|
+      t.references :table, foreign_key: true
+      t.references :column, foreign_key: true
+      t.references :user, foreign_key: true
+      t.text :content
+
+      t.timestamps
+    end
+  end
+end
