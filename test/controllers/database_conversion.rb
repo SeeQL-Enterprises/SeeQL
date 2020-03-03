f3a53214f16e27b0f1cc5724d23ecc3c11dbee73
Bruncky <brunobrunckhorst@gmail.com>
2020-03-03 14:41:14 +0100

    Wrote method to parse table headers and columns into JSON
    Written in test file

--- test/controllers/database_conversion.rb
+++ test/controllers/database_conversion.rb
@@ -1,1 +1,13 @@
-database_conversion.rb
+require 'json'
+
+table_headers = ActiveRecord::Base.connection.tables
+
+table_headers.each do |table|
+  tables = ["#{table}"]
+
+  ActiveRecord::Base.connection.columns(table).map do |column|
+    tables << column.name
+  end
+
+  puts tables.to_json
+end
