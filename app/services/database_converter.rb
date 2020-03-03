0156b21d7b5030c73e5fdf15a98fa8c39dd39140
Bruncky <brunobrunckhorst@gmail.com>
2020-03-03 15:15:09 +0100

    Transfered function into database_converter.rb
    Deleted unnecessary test filee

--- app/services/database_converter.rb
+++ app/services/database_converter.rb
@@ -1,9 +1,23 @@
+require 'json'
+
 class DatabaseConverter
+  # This SO takes in a database and converts all tables and its respective columns into a JSON object
   def initiaize(database_instance)
     @database = database_instance
   end
 
   def convert
+    table_headers = ActiveRecord::Base.connection.tables
+    table_hash = {}
 
+    table_headers.each do |table|
+      table_hash["#{table}"] = []
+
+      ActiveRecord::Base.connection.columns(table).each do |column|
+        table_hash["#{table}"] << column.name
+      end
+    end
+
+    table_hash.to_json
   end
 end
