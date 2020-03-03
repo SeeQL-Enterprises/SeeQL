981b76ff682d90ed4bcb5fc13906e1c859ed4ff9
Finn Rub <r.finn40@yahoo.com>
2020-03-03 11:59:45 +0100

    add projects show and show method

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -7,12 +7,16 @@ class ProjectsController < ApplicationController
 
   def create
     @project = Project.new(project_params)
-    @project.user
+    @project.user = current_user
     if @project.save
-      redirect_to project_path
+      redirect_to project_path(@project)
     else
       render :new
     end
+  end
+
+  def show
+    @project = Project.find(params[:id])
   end
 
   private
