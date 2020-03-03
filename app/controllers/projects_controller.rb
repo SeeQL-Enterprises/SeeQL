16ebd9807ed5582004963500173d33105b9afd83
Finn Rub <r.finn40@yahoo.com>
2020-03-03 11:36:07 +0100

    add new and create method

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,22 @@
 class ProjectsController < ApplicationController
+
+  def new
+    @project = Project.new
+    raise
+  end
+
+  def create
+    @project = Project.new(project_params)
+    @project.user
+    if @project.save
+      redirect_to project_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def project_params
+    params.require(:project).permit(:name)
+  end
 end
