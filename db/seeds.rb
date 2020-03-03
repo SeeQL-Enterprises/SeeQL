376bc572b2f805896da665cd0d530885989291de
Patrick Plue <pluepatrick@gmail.com>
2020-03-03 14:22:20 +0100

    g database controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,42 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "destroying projects..."
+Project.destroy_all
+
+puts "destroying users..."
+User.destroy_all
+
+puts "generate project owner..."
+user = User.create!({
+email: "finn@gmail.com",
+password: "123456"
+ })
+
+project = Project.new({
+  name: "test"
+})
+
+project.user = user
+project.save
+
+
+
+
+
+puts "generate users to collaborate..."
+email = [
+"tim@gmail.com",
+"bruno@gmail.com",
+"patrick@gmail.com"
+]
+i = 0
+
+3.times do
+  user = User.new({
+    email: email[i], password: "123456"
+  })
+  user.save!
+  collaborator = Collaborator.new({
+  })
+  collaborator.user = user
+  project.user = user
+  i += 1
+end
