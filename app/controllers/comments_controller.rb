4a4ac0e43a6b939bfdc3179f42a6bfff15d9c358
Finn Rub <r.finn40@yahoo.com>
2020-03-04 13:05:51 +0100

    add migration with usere ref in comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,11 +2,12 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.user = current_user
+    @comment.table = params[:table_id]
   end
 
   private
 
   def comment_params
-    params.require[:comment].permit(:content)
+    params.require(:comment).permit(:content)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_144704) do
+ActiveRecord::Schema.define(version: 2020_03_04_120428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,7 +39,9 @@ ActiveRecord::Schema.define(version: 2020_03_03_144704) do
     t.datetime "updated_at", null: false
     t.string "commentable_type"
     t.bigint "commentable_id"
+    t.bigint "user_id"
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "databases", force: :cascade do |t|
@@ -82,6 +84,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_144704) do
   add_foreign_key "collaborators", "projects"
   add_foreign_key "collaborators", "users"
   add_foreign_key "columns", "tables"
+  add_foreign_key "comments", "users"
   add_foreign_key "databases", "projects"
   add_foreign_key "projects", "users"
   add_foreign_key "tables", "databases"
