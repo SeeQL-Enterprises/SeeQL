af19e9f08e75aab36544ba7ba47bb3bbece35df0
Patrick Plue <pluepatrick@gmail.com>
2020-03-04 15:39:47 +0100

    applied a 3d shadow to the tables

--- app/javascript/plugins/interactjs.js
+++ app/javascript/plugins/interactjs.js
@@ -1,42 +1,37 @@
-import interact from 'interactjs'
+import interact from "interactjs";
 
 // target elements with the "draggable" class
-interact('.draggable')
-  .draggable({
-    // enable inertial throwing
-    inertia: true,
-    // keep the element within the area of it's parent
-    modifiers: [
-      interact.modifiers.restrictRect({
-        restriction: 'parent',
-        endOnly: true
-      })
-    ],
-    // enable autoScroll
-    autoScroll: true,
+interact(".draggable").draggable({
+  // enable inertial throwing
+  inertia: true,
+  preventCollision: true,
+  // keep the element within the area of it's parent
+  // modifiers: [
+  //   interact.modifiers.restrictRect({
+  //     restriction: 'parent',
+  //     endOnly: true
+  //   })
+  // ],
+  // enable autoScroll
+  autoScroll: true,
 
-    listeners: {
-      // call this function on every dragmove event
-      move: dragMoveListener,
+  listeners: {
+    // call this function on every dragmove event
+    move: dragMoveListener
+  }
+});
 
-    }
-  })
-
-function dragMoveListener (event) {
-  var target = event.target
+function dragMoveListener(event) {
+  var target = event.target;
   // keep the dragged position in the data-x/data-y attributes
-  var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
-  var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+  var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
+  var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
 
   // translate the element
-  target.style.webkitTransform =
-    target.style.transform =
-      'translate(' + x + 'px, ' + y + 'px)'
+  target.style.webkitTransform = target.style.transform =
+    "translate(" + x + "px, " + y + "px)";
 
   // update the posiion attributes
-  target.setAttribute('data-x', x)
-  target.setAttribute('data-y', y)
+  target.setAttribute("data-x", x);
+  target.setAttribute("data-y", y);
 }
-
-// this function is used later in the resizing and gesture demos
-window.dragMoveListener = dragMoveListener
