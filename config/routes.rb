748980755b701a548651cdabfe123bf8db955d93
Finn Rub <r.finn40@yahoo.com>
2020-03-04 12:33:15 +0100

    changed comment new to use active record not json

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
     resources :databases, only: [:index]
   end
   resources :databases, only: :show
-
+  resources :tables, only: [] do
+    resources :comments, only: :create
+  end
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
