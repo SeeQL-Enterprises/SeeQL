3fdb08fcb2f3a1b09274daeca9b90c7a38f18149
Patrick Plue <pluepatrick@gmail.com>
2020-03-04 11:49:58 +0100

    added the draggable js libary

---
+++ app/javascript/plugins/interactjs.js
@@ -0,0 +1,18 @@
+import interact from 'interactjs'
+
+const position = { x: 0, y: 0 }
+
+interact('.draggable').draggable({
+  listeners: {
+    start (event) {
+      console.log(event.type, event.target)
+    },
+    move (event) {
+      position.x += event.dx
+      position.y += event.dy
+
+      event.target.style.transform =
+        `translate(${position.x}px, ${position.y}px)`
+    },
+  }
+})
