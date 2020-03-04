551207ae05105ca74f72e5afa866f0b6aa2b9a6e
Bruncky <brunobrunckhorst@gmail.com>
2020-03-04 15:04:04 +0100

    Changed data_type from Integer to String

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_04_111532) do
+ActiveRecord::Schema.define(version: 2020_03_04_140232) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,7 +27,7 @@ ActiveRecord::Schema.define(version: 2020_03_04_111532) do
   create_table "columns", force: :cascade do |t|
     t.bigint "table_id"
     t.string "name"
-    t.integer "data_type"
+    t.string "data_type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["table_id"], name: "index_columns_on_table_id"
