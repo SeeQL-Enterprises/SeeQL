6568ded54d2745d69e27d9f24f3ef189fa056ab8
Tim Vierboom <tim-vierboom@gmx.de>
2020-03-04 12:34:17 +0100

    display checkboxes for tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_03_144704) do
+ActiveRecord::Schema.define(version: 2020_03_04_111532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2020_03_03_144704) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "display", default: false
     t.index ["database_id"], name: "index_tables_on_database_id"
   end
 
