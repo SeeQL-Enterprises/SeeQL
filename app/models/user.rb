9fb1b8b8989506094e48dbf625af4aae3845831e
Bruncky <brunobrunckhorst@gmail.com>
2020-03-05 15:47:04 +0100

    Readability

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
   has_many :projects
   has_many :comments, as: :commentable
 end
