03599a7f9250f92035efb70e1966e2d0d03ed8cf
Bruncky <brunobrunckhorst@gmail.com>
2020-03-06 22:04:57 +0100

    Slight change to method that reads foreign keys from columns

--- app/services/database_accessor.rb
+++ app/services/database_accessor.rb
@@ -64,7 +64,20 @@ class DatabaseAccessor
   def save_columns(table)
     columns = @connection.exec "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '#{table.name}'"
     foreign_keys = @connection.exec "
-    SELECT tc.table_name AS from_table, kcu.column_name AS from_column, ccu.table_name AS to_table FROM information_schema.table_constraints AS tc JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name AND tc.table_schema = kcu.table_schema JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name AND ccu.table_schema = tc.table_schema WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.table_name='tables'"
+    SELECT
+    tc.table_name AS from_table,
+    kcu.column_name AS from_column,
+    ccu.table_name AS to_table
+    FROM
+    information_schema.table_constraints AS tc
+      JOIN information_schema.key_column_usage AS kcu
+      ON tc.constraint_name = kcu.constraint_name
+      AND tc.table_schema = kcu.table_schema
+      JOIN information_schema.constraint_column_usage AS ccu
+      ON ccu.constraint_name = tc.constraint_name
+      AND ccu.table_schema = tc.table_schema
+    WHERE tc.constraint_type = 'FOREIGN KEY'
+    AND tc.table_name='#{table.name}"
 
     columns.each do |column|
       column = Column.new(name: column["column_name"], datatype: column["data_type"])
