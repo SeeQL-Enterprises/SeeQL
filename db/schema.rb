b72cb8596e9f853088651c1bc9deec7ab6fa579d
Bruncky <brunobrunckhorst@gmail.com>
2020-03-06 22:45:20 +0100

    Migrated DB

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,16 @@
-ActiveRecord::Schema.define(version: 2020_03_06_210350) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_03_06_214201) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +30,6 @@ ActiveRecord::Schema.define(version: 2020_03_06_210350) do
     t.string "datatype"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.json "foreign_keys"
     t.index ["table_id"], name: "index_columns_on_table_id"
   end
 
@@ -55,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_03_06_210350) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "display", default: false
+    t.json "foreign_keys"
     t.index ["database_id"], name: "index_tables_on_database_id"
   end
 
