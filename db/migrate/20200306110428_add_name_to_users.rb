8a638afb23203f2a4e92ed3c8dc70924802dab20
Finn Rub <r.finn40@yahoo.com>
2020-03-06 12:05:00 +0100

    fix migration

--- db/migrate/20200306110428_add_name_to_users.rb
+++ db/migrate/20200306110428_add_name_to_users.rb
@@ -1,5 +1,5 @@
 class AddNameToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :users, :string
+    add_column :users, :name, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_105038) do
+ActiveRecord::Schema.define(version: 2020_03_06_110428) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 2020_03_06_105038) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "users"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
