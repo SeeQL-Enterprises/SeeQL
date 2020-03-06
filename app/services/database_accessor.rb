8b269a5caa433fe4b863524d6eb4784dbad42546
Bruncky <brunobrunckhorst@gmail.com>
2020-03-06 22:38:31 +0100

    Refactored the query for better readability

--- app/services/database_accessor.rb
+++ app/services/database_accessor.rb
@@ -20,7 +20,7 @@ class DatabaseAccessor
       save_database(tables)
     rescue PG::Error => error
 
-      puts "Error, fam: #{error.message}"
+      puts "#{error.message}"
 
     ensure
 
@@ -65,19 +65,18 @@ class DatabaseAccessor
     columns = @connection.exec "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '#{table.name}'"
     foreign_keys = @connection.exec "
     SELECT
-    tc.table_name AS from_table,
-    kcu.column_name AS from_column,
-    ccu.table_name AS to_table
-    FROM
-    information_schema.table_constraints AS tc
-      JOIN information_schema.key_column_usage AS kcu
+      tc.table_name AS from_table,
+      kcu.column_name AS from_column,
+      ccu.table_name AS to_table
+    FROM information_schema.table_constraints AS tc
+    JOIN information_schema.key_column_usage AS kcu
       ON tc.constraint_name = kcu.constraint_name
       AND tc.table_schema = kcu.table_schema
-      JOIN information_schema.constraint_column_usage AS ccu
+    JOIN information_schema.constraint_column_usage AS ccu 
       ON ccu.constraint_name = tc.constraint_name
       AND ccu.table_schema = tc.table_schema
     WHERE tc.constraint_type = 'FOREIGN KEY'
-    AND tc.table_name='#{table.name}"
+    AND tc.table_name = '#{table.name}'"
 
     columns.each do |column|
       column = Column.new(name: column["column_name"], datatype: column["data_type"], foreign_keys: foreign_keys)
