09e0ab5bd1745a578666d22c37d4589b004fa4c4
Bruncky <brunobrunckhorst@gmail.com>
2020-03-06 15:37:21 +0100

    Created databases#create

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -4,4 +4,14 @@ class DatabasesController < ApplicationController
     @database = Database.find(params[:id])
     @tables = @database.tables
   end
+
+  def create
+    DatabaseAccessor.new(database_params)
+  end
+
+  private
+
+  def database_params
+    params.require(:database).permit(:host, :db_name, :user, :password)
+  end
 end
