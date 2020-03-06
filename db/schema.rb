0287adb0a91a61cfd42d6c43964412be4c0a39af
Bruncky <brunobrunckhorst@gmail.com>
2020-03-06 22:10:26 +0100

    Migrated DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_110428) do
+ActiveRecord::Schema.define(version: 2020_03_06_210350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_03_06_110428) do
     t.string "datatype"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.json "foreign_keys"
     t.index ["table_id"], name: "index_columns_on_table_id"
   end
 
