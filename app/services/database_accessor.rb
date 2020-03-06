9eff7db80249712df6c398604f9d219c35ba852f
Bruncky <brunobrunckhorst@gmail.com>
2020-03-06 15:26:39 +0100

    Added a query to get foreign keys

---
+++ app/services/database_accessor.rb
@@ -0,0 +1,76 @@
+require 'pg'
+
+class DatabaseAccessor
+  # This Service Object takes in a database (of class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter)
+  # and converts all tables and its respective columns into a JSON object
+  def initialize(options = {})
+    @host = options[:host]
+    @db_name = options[:db_name]
+    @user = options[:user]
+    @password = options[:password]
+  end
+
+  def call
+    begin
+      @connection = PG.connect host: @host, dbname: @db_name, user: @user, password: @password
+
+      # Each table is a hash where the key is "table_name" and the value is the actual name of the table
+      tables = @connection.exec "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'"
+
+      save_database(tables)
+    rescue PG::Error => error
+
+      puts "Error, fam: #{error.message}"
+
+    ensure
+
+      @connection.close if @connection
+
+    end
+  end
+
+  def save_database(tables)
+    user = User.create!(email: "THIS@gmail.com", password: "123456")
+    project = Project.create!(name: "THIS PROJECT", user: user)
+
+    database = Database.new(name: @connection.db, project: project)
+
+    if save_tables(database, tables)
+      if database.save
+        puts "SUCCESS: DB saved"
+      else
+        puts "ERROR: DB not saved"
+      end
+    end
+  end
+
+  def save_tables(database, tables)
+    tables.each do |key, _|
+      key.each do |_ , value|
+        table = Table.new(name: value)
+        table.database = database
+
+        if save_columns(table)
+          table.save
+          true
+        else
+          false
+        end
+
+      end
+    end
+  end
+
+  def save_columns(table)
+    columns = @connection.exec "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = '#{table.name}'"
+    foreign_keys = @connection.exec "
+    SELECT tc.table_name AS from_table, kcu.column_name AS from_column, ccu.table_name AS to_table FROM information_schema.table_constraints AS tc JOIN information_schema.key_column_usage AS kcu ON tc.constraint_name = kcu.constraint_name AND tc.table_schema = kcu.table_schema JOIN information_schema.constraint_column_usage AS ccu ON ccu.constraint_name = tc.constraint_name AND ccu.table_schema = tc.table_schema WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.table_name='tables'"
+
+    columns.each do |column|
+      column = Column.new(name: column["column_name"], datatype: column["data_type"])
+      column.table = table
+
+      column.save ? true : false
+    end
+  end
+end
