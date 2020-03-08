88ba62a7f2dd315ad59f554e3e962e5df003947c
Patrick Plue <pluepatrick@gmail.com>
2020-03-08 18:13:52 +0100

    added funtion to remember the position of the card

--- app/javascript/plugins/interactjs.js
+++ app/javascript/plugins/interactjs.js
@@ -4,7 +4,6 @@ import interact from "interactjs";
 interact(".draggable").draggable({
   // enable inertial throwing
   inertia: true,
-  preventCollision: true,
   // keep the element within the area of it's parent
   // modifiers: [
   //   interact.modifiers.restrictRect({
@@ -21,6 +20,38 @@ interact(".draggable").draggable({
   }
 });
 
+function restorePositions() {
+  const data = localStorage.getItem("position") || "{}";
+  const currentData = JSON.parse(data);
+  const items = document.querySelectorAll(".draggable");
+
+  items.forEach((target) => {
+    const id = target.getAttribute("data-id");
+
+    if (currentData[id]) {
+      const positions = currentData[id].split(":");
+      const x = positions[0];
+      const y = positions[1];
+
+      target.style.webkitTransform = target.style.transform =
+        "translate(" + x + "px, " + y + "px)";
+
+      target.setAttribute("data-x", x);
+      target.setAttribute("data-y", y);
+    }
+  });
+}
+
+restorePositions();
+
+function rememberPosition(target) {
+  const data = localStorage.getItem("position") || "{}";
+  const currentData = JSON.parse(data);
+  currentData[target.getAttribute("data-id")] =
+    target.getAttribute("data-x") + ":" + target.getAttribute("data-y");
+  localStorage.setItem("position", JSON.stringify(currentData));
+}
+
 function dragMoveListener(event) {
   var target = event.target;
   // keep the dragged position in the data-x/data-y attributes
@@ -34,4 +65,6 @@ function dragMoveListener(event) {
   // update the posiion attributes
   target.setAttribute("data-x", x);
   target.setAttribute("data-y", y);
+
+  rememberPosition(target);
 }
