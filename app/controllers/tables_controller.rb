2988246bbdb8e46d02703a50c24209dfa835be11
Finn Rub <r.finn40@yahoo.com>
2020-03-09 12:01:51 +0100

    can add table with columns dinamically

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -9,9 +9,26 @@ class TablesController < ApplicationController
     @column = Column.new
   end
 
-  def create
+  def new
     @table = Table.new
-    @column = Column.new
+    @table.columns.build
+  end
+
+  def create
+    @table = Table.new(table_params)
+    authorize @table
+    @database = Database.find(params[:database_id])
+    @table.database = @database
+    columns_params = table_params[:columns_attributes]
+    @table.save!
+    redirect_to database_tables_path
+  end
+
+  private
+  def table_params
+    params.require(:table).permit(:name, columns_attributes: Column.attribute_names.map(&:to_sym).push(:_destroy))
   end
 
 end
+
+#
