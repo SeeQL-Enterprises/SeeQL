10494a75df859006a5b8240fbe4a554c5c01024a
Tim Vierboom <tim-vierboom@gmx.de>
2020-03-10 16:09:34 +0100

    add select all button

--- app/javascript/plugins/select_all.js
+++ app/javascript/plugins/select_all.js
@@ -1,29 +1,39 @@
-// $('#select_all').click(function() {
-//     $('input[type="checkbox"]').prop('checked', true);
-// });
-
-// $('#unselect_all').click(function() {
-//     $('input[type="checkbox"]').prop('checked', false);
-// });
+selectAllBtn = document.querySelector('#select_all');
 
 buttons = document.querySelectorAll(".form-check");
 buttons.forEach((button) => {
   button.addEventListener("click", function(event) {
     button.classList.toggle("on");
-  })});
+  })
+});
 
+const changeVisiblesOn = () => {
+  buttons = document.querySelectorAll(".form-check");
+  buttons.forEach((button) => {
+      button.classList.add("on");
+  });
+}
 
-// selectAllBtn = document.querySelector('#select_all');
-// toggles = document.querySelectorAll('.form-check');
+const changeVisiblesOff = () => {
+  buttons = document.querySelectorAll(".form-check");
+  buttons.forEach((button) => {
+      button.classList.remove("on");
+  });
+}
 
-// toggles.forEach((toggle) => {
-//   selectAllBtn.addEventListener("click", () => {
-//   if (toggle.checked === true) {
-//     toggle.classList.toggle(false);
-//   } else {
-//     toggle.classList.toggle(true);
-// }})});
+const selectAllInputs = () => {
+  const toggles = document.querySelectorAll('.form-check-input')
+  toggles.forEach((toggle) =>{
+    if (toggle.checked === true) {
+      toggle.checked = false
+      selectAllBtn.innerText = 'Select all'
+      changeVisiblesOff();
+    }else {
+      toggle.checked = true
+      selectAllBtn.innerText = 'Unselect all'
+      changeVisiblesOn();
+    }
+  })
+}
 
-
-
-
+selectAllBtn.addEventListener('click', selectAllInputs)
