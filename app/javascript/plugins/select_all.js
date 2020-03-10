723d3c9904c253cf0b43c9b311783e01157e2d36
Tim Vierboom <tim-vierboom@gmx.de>
2020-03-10 13:37:31 +0100

    add a toggle button for table selection

--- app/javascript/plugins/select_all.js
+++ app/javascript/plugins/select_all.js
@@ -1,7 +1,26 @@
-$('#select_all').click(function() {
-    $('input[type="checkbox"]').prop('checked', true);
-});
+// $('#select_all').click(function() {
+//     $('input[type="checkbox"]').prop('checked', true);
+// });
 
-$('#unselect_all').click(function() {
-    $('input[type="checkbox"]').prop('checked', false);
-});
+// $('#unselect_all').click(function() {
+//     $('input[type="checkbox"]').prop('checked', false);
+// });
+
+// selectAllBtn = document.querySelector('#select_all')
+// checkbox = document.querySelector('.form-check-input')
+
+// selectAllBtn.addEventListener("click", () => {
+//   checkbox.classList.toggle(true);
+//     if (checkbox.checked === true) {
+//     (checkbox.checked === false);
+//   } else {
+//     (checkbox.checked === true);
+//   }});
+
+buttons = document.querySelectorAll(".form-check");
+buttons.forEach((button) => {
+  button.addEventListener("click", function(event) {
+    button.classList.toggle("on");
+  })});
+
+
