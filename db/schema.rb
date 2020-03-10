a75feeefff72eddfd16bca166898b59e6d32a8f9
Finn Rub <r.finn40@yahoo.com>
2020-03-10 12:52:39 +0100

    add edit boolean to columns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_214201) do
+ActiveRecord::Schema.define(version: 2020_03_10_114600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_03_06_214201) do
     t.string "datatype"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "edit", default: false
     t.index ["table_id"], name: "index_columns_on_table_id"
   end
 
