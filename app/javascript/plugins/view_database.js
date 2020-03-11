419a542bd1c6662099c237ac66d1d6366314716d
Bruncky <brunobrunckhorst@gmail.com>
2020-03-11 14:56:43 +0100

    Fixed typo

--- app/javascript/plugins/view_database.js
+++ app/javascript/plugins/view_database.js
@@ -4,6 +4,6 @@ const addDatabaseButton = document.getElementById("add-database-button");
 
 if (viewDatabaseButton) {
     // Toggle SCSS classes to make "add database" go gray
-    addDatabaseButton.classList.remove("btn-treehosue");
+    addDatabaseButton.classList.remove("btn-treehouse");
     addDatabaseButton.classList.add("btn-cancel");
 }
