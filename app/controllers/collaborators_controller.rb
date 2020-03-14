640838348ade71c678805de7451e2eb3ee5a839b
Bruncky <brunobrunckhorst@gmail.com>
2020-03-14 20:23:58 +0100

    Changed Add Collaborator form to use e-mails rather than a dropdown

--- app/controllers/collaborators_controller.rb
+++ app/controllers/collaborators_controller.rb
@@ -1,25 +1,26 @@
 class CollaboratorsController < ApplicationController
-  def create
-    @collaborator = Collaborator.new
-    authorize @collaborator
-    @collaborator.user = set_user
-    @project = Project.find(params[:project_id])
-    @collaborator.project = @project
-    if @collaborator.save
-      redirect_to project_path(@project)
-    else
-      render 'projects/show'
+    def create
+        @collaborator = Collaborator.new
+        authorize @collaborator
+        @collaborator.user = set_user
+        
+        @project = Project.find(params[:project_id])
+        @collaborator.project = @project
+
+        if @collaborator.save
+            redirect_to project_path(@project)
+        else
+            render 'projects/show'
+        end
     end
 
-  end
+    private
+    def collaborator_params
+        params.require(:collaborator).permit(:user)
+    end
 
-  private
-  def collaborator_params
-    params.require(:collaborator).permit(:user)
-  end
-
-  def set_user
-    User.find(collaborator_params[:user])
-  end
+    def set_user
+        User.find_by_email(collaborator_params[:user])
+    end
 
 end
