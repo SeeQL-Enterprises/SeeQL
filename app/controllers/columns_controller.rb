c604bd1bc0744d660ce925fd755301966671ce67
Bruncky <brunobrunckhorst@gmail.com>
2020-03-19 12:21:29 +0100

    columns_controller cleanup

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -1,41 +1,46 @@
 class ColumnsController < ApplicationController
 
-  def create
-    @column = Column.new(column_params)
-    authorize @column
-    @database = @column.table.database
-    @column.save!
-    respond_to do |format|
-      format.js {
-        @counter = params[:column_proxy][:counter]
-        @table = @column.table
-        }
-      format.html { redirect_to database_tables_path(@database) }
+    def create
+        @column = Column.new(column_params)
+        authorize @column
+        @database = @column.table.database
+
+        @column.save!
+
+        respond_to do |format|
+            format.js {
+                @counter = params[:column_proxy][:counter]
+                @table = @column.table
+            }
+
+            format.html { redirect_to database_tables_path(@database) }
+        end
+
     end
 
-  end
+    def destroy
+        @column = Column.find(params[:id])
+        @database = @column.table.database
+        authorize @column
 
-  def destroy
-    @column = Column.find(params[:id])
-    @database = @column.table.database
-    authorize @column
-    if @column.edit
-      @column.destroy
-      respond_to do |format|
-        format.js
-        format.html { redirect_to database_tables_path(@database) }
-      end
-    else
-      respond_to do |format|
-        format.js
-        format.html { redirect_to database_tables_path(@database) }
-      end
+        if @column.edit
+            @column.destroy
+
+            respond_to do |format|
+                format.js
+                format.html { redirect_to database_tables_path(@database) }
+            end
+        else
+            respond_to do |format|
+                format.js
+                format.html { redirect_to database_tables_path(@database) }
+            end
+        end
     end
-  end
 
-  private
+    private
 
-  def column_params
-    params.require(:column_proxy).permit(:name, :datatype, :table_id, :edit)
-  end
+    def column_params
+        params.require(:column_proxy).permit(:name, :datatype, :table_id, :edit)
+    end
 end
