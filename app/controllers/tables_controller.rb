d3252c725f18e6ecca96386ead45d40d4691735c
Bruncky <brunobrunckhorst@gmail.com>
2020-03-19 12:24:11 +0100

    tables_controller cleanup

--- app/controllers/tables_controller.rb
+++ app/controllers/tables_controller.rb
@@ -1,34 +1,35 @@
 class TablesController < ApplicationController
-  def index
-    @comment = Comment.new
-    @project = policy_scope(Project)
-    @database = Database.find(params[:database_id])
-    @tables = @database.tables.displayed
-    @columns = @tables.columns
-    @table = Table.new
-    @column = Column.new
-  end
+    def index
+        @comment = Comment.new
+        @project = policy_scope(Project)
+        @database = Database.find(params[:database_id])
+        @tables = @database.tables.displayed
+        @columns = @tables.columns
+        @table = Table.new
+        @column = Column.new
+    end
 
-  def new
-    @table = Table.new
-    @table.columns.build
-  end
+    def new
+        @table = Table.new
+        @table.columns.build
+    end
 
-  def create
-    @table = Table.new(table_params)
-    authorize @table
-    @database = Database.find(params[:database_id])
-    @table.database = @database
-    columns_params = table_params[:columns_attributes]
-    @table.save!
-    redirect_to database_tables_path
-  end
+    def create
+        @table = Table.new(table_params)
+        authorize @table
 
-  private
-  def table_params
-    params.require(:table).permit(:name, :edit, :display, columns_attributes: Column.attribute_names.map(&:to_sym).push(:_destroy))
-  end
+        @database = Database.find(params[:database_id])
+        @table.database = @database
+        columns_params = table_params[:columns_attributes]
+
+        @table.save!
+
+        redirect_to database_tables_path
+    end
+
+    private
+    def table_params
+        params.require(:table).permit(:name, :edit, :display, columns_attributes: Column.attribute_names.map(&:to_sym).push(:_destroy))
+    end
 
 end
-
-#
