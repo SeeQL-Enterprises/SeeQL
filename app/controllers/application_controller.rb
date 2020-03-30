c79fbd47e1cb01a23ceac11110d98e4e262ce147
Bruncky <brunobrunckhorst@gmail.com>
2020-03-30 15:11:24 +0200

    Added Devise sanitizer to allow avatars on account update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   include Pundit
 
@@ -17,8 +18,13 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password])
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :avatar])
+  end
+
   def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+      devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   end
 
   def default_url_options
