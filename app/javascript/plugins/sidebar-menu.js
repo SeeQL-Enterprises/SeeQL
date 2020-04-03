ecd84b9c156d3fb78fc67370067920cab73bcbb3
Tim Vierboom <tim-vierboom@gmx.de>
2020-04-03 18:52:39 +0200

    fix burger menu animation

--- app/javascript/plugins/sidebar-menu.js
+++ app/javascript/plugins/sidebar-menu.js
@@ -1,7 +1,7 @@
-    let menu = document.querySelector( "#mySidenav" );
-    let menuIcon = document.querySelector( "#my-cancel-lines" );
-     let bodyBackground = document.querySelector( "#body" );
-    menuIcon.addEventListener( "click",()=> {
-      menu.classList.contains('open') ? menu.classList.remove('open') : menu.classList.add('open');
-    });
+    // let menu = document.querySelector( "#mySidenav" );
+    // let menuIcon = document.querySelector( "#my-cancel-lines" );
+    //  let bodyBackground = document.querySelector( "#body" );
+    // menuIcon.addEventListener( "click",()=> {
+    //   menu.classList.contains('open') ? menu.classList.remove('open') : menu.classList.add('open');
+    // });
 
