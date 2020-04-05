2736f6cfa7e6955103ea210ff5d9698f207c244d
Bruncky <brunobrunckhorst@gmail.com>
2020-04-05 20:23:30 +0200

    Rails 6.0.0 upgrade

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,27 @@
 Rails.application.routes.draw do
-  resources :databases, only: [:show]
-  devise_for :users
-  resources :projects, only: [:new, :create, :show, :index] do
-    resources :collaborators, only: [:new, :create]
-    resources :databases, only: [:index, :create]
-  end
+    resources :databases, only: [:show]
 
-  resources :databases, only: [] do
-    resources :tables, only: [:index, :create] do
-      resources :comments, only: :create
+    devise_for :users
+
+    resources :projects, only: [:new, :create, :show, :index] do
+        resources :collaborators, only: [:new, :create]
+        resources :databases, only: [:index, :create]
     end
-  end
 
-  resources :database_table_preferences, only: :update
+    resources :databases, only: [] do
+        resources :tables, only: [:index, :create] do
+            resources :comments, only: :create
+        end
+    end
 
-  resources :columns, only: [:create, :destroy] do
-    resources :comments, only: :create
-  end
+    resources :database_table_preferences, only: :update
 
-  resources :comments, only: :destroy
+    resources :columns, only: [:create, :destroy] do
+        resources :comments, only: :create
+    end
 
-  root to: 'pages#home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    resources :comments, only: :destroy
+
+    root to: 'pages#home'
+    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
