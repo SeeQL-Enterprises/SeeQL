22aa7075d35911afc915a8f4f1e5a02e9d68b7db
Bruncky <brunobrunckhorst@gmail.com>
2020-04-27 19:57:11 +0200

    Added MySQL Policy

---
+++ app/policies/mysql_accessor_policy.rb
@@ -0,0 +1,11 @@
+class MySQLAccessorPolicy < ApplicationPolicy
+    class Scope < Scope
+        def resolve
+            scope.all
+        end
+    end
+
+    def create?
+        true
+    end
+end
