c7c569c36f39ccedfc244ec642488a2fe68afa40
Bruncky <brunobrunckhorst@gmail.com>
2020-06-05 01:20:51 +0200

    Added comments for #documentation

--- app/javascript/plugins/clipboard.js
+++ app/javascript/plugins/clipboard.js
@@ -1,12 +1,15 @@
+// This makes sure we have imported the initPopover function
 import { initPopover } from '../plugins/popover';
 
 const clipboardButton = document.getElementById("clipboard-button")
 
+// This function handles copying a given String to the clipboard, it takes the String as a parameter
 function copyTextToClipboard(text) {
     navigator.clipboard.writeText(text).then(
         function () {
             console.log("Async: Copying to clipboard was successful!");
 
+            // Initializes the popover only when necessary and then sets "data-content"
             initPopover();
             clipboardButton.setAttribute("data-content", "URL has been copied to clipboard!");
 
@@ -15,6 +18,7 @@ function copyTextToClipboard(text) {
         function (err) {
             console.error("Async: Could not copy text: ", err);
 
+            // Same as above, but with an error message
             initPopover();
             clipboardButton.setAttribute("data-content", "URL could not be copied to clipboard!");
 
@@ -23,6 +27,7 @@ function copyTextToClipboard(text) {
     );
 }
 
+// Calling the above function with our URL as the String
 clipboardButton.addEventListener("click", function (event) {
     copyTextToClipboard("https://www.seeql.dev/");
 });
