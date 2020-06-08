4aace50f1b5a5a52749a932834b54d453522a377
Bruncky <brunobrunckhorst@gmail.com>
2020-06-08 11:58:39 +0200

    Added :confirmable to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+    # :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable
 
     has_many :projects
     has_many :comments, as: :commentable
