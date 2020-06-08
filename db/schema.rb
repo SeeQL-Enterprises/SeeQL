847c3ff890d422041bd6ad8ade308df1c2f6f7d5
Bruncky <brunobrunckhorst@gmail.com>
2020-06-08 11:27:39 +0200

    Migrated DB with new lines

--- db/schema.rb
+++ db/schema.rb
@@ -99,6 +99,13 @@ ActiveRecord::Schema.define(version: 2020_04_05_182202) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.integer "failed_attempts", default: 0, null: false
+    t.string "unlock_token"
+    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
