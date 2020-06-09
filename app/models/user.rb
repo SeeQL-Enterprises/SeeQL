ce19720969bed4720f4f71253f6aefd1b950c650
Bruncky <brunobrunckhorst@gmail.com>
2020-06-09 20:52:44 +0200

    Made users omniauthable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
     # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+    # :confirmable, :lockable, :timeoutable, :trackable
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
 
     has_many :projects
     has_many :comments, as: :commentable
