e8bdb3c77dedbef4a8fc906d6906238177347b24
Bruncky <brunobrunckhorst@gmail.com>
2020-06-09 21:14:20 +0200

    Implemented an OAuth method in User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,15 @@ class User < ApplicationRecord
     has_many :comments, as: :commentable
 
     has_one_attached :avatar
+
+    def self.from_omniauth(auth)
+        where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+            user.email = auth.info.email
+            user.password = Devise.friendly_token[0, 20]
+            user.name = auth.info.name
+            user.avatar = auth.info.image
+
+            user.skip_confirmation!
+        end
+    end
 end
