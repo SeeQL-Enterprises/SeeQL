1ac39423c42393705de5486141ed7faf10d51403
Bruncky <brunobrunckhorst@gmail.com>
2020-06-09 14:15:02 +0200

    Configured sending of password and e-mail change e-mails

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -117,10 +117,10 @@ Devise.setup do |config|
   # config.pepper = '2a13675ed4a1c57d9f39952952e62073cbe19758c1f098d3467d00fefa1e4ac755853adae80ed84b7842b1c5b0a20ad2fcaec91821834372544d6e91964a48e4'
 
   # Send a notification to the original email when the user's email is changed.
-  # config.send_email_changed_notification = false
+  config.send_email_changed_notification = true
 
   # Send a notification email when the user's password is changed.
-  # config.send_password_change_notification = false
+  config.send_password_change_notification = true
 
   # ==> Configuration for :confirmable
   # A period that the user is allowed to access the website even without
