096c60bf97b3b8854a3b755341f92014454a9da6
Bruncky <brunobrunckhorst@gmail.com>
2020-06-09 21:10:44 +0200

    Defined the Controller that will handle callbacks

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,2 +1,17 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+    def github
+        @user = User.from_omniauth(request.env['omniauth.auth'])
+
+        if @user.persisted?
+            sign_in_and_redirect @user
+            set_flash_message(:notice, :success, kind: 'GitHub') if is_navigational_format?
+        else
+            session['devise.github_data'] = request.env['omniauth.auth']
+            redirect_to projects_path
+        end
+    end
+
+    def failure
+        redirect_to root_path
+    end
 end
