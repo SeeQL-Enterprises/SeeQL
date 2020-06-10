0e84504a57ab21a94db2364f436a198d7609d4a6
Bruncky <brunobrunckhorst@gmail.com>
2020-06-11 00:30:49 +0200

    Fixed missing info according to new encryption standards

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ require 'open-uri'
 
 class User < ApplicationRecord
     # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable, :trackable
-    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: %i[github]
+    # :lockable, :timeoutable, :trackable
+    devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, :confirmable, :omniauthable, omniauth_providers: %i[github]
 
     has_many :projects
     has_many :comments, as: :commentable
@@ -12,9 +12,11 @@ class User < ApplicationRecord
 
     def self.from_omniauth(auth)
         where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+            user.provider = auth.provider
+            user.uid = auth.uid
+            user.name = auth.info.name
             user.email = auth.info.email
             user.password = Devise.friendly_token[0, 20]
-            user.name = auth.info.name
 
             user_avatar = URI.open(auth.info.image)
             user.avatar.attach(io: user_avatar, filename: 'avatar')
