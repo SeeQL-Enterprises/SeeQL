b79b4a235edd0a0efdae9cc825051f8ab089df77
Bruncky <brunobrunckhorst@gmail.com>
2020-06-12 14:10:21 +0200

    HOTFIX: Adapted GitHub OAuth to use first_name and last_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,9 +12,17 @@ class User < ApplicationRecord
 
     def self.from_omniauth(auth)
         where(provider: auth.provider, uid: auth.uid).first_or_create! do |user|
+            # This takes the provider's name (here GitHub) and saves that info for devise
+            # Same with UID (UserID)
             user.provider = auth.provider
             user.uid = auth.uid
-            user.name = auth.info.name
+
+            # GitHub only has 'name', so here we separate the names manually
+            # Might not be perfect (some have 2 first names), but users can always edit that
+            separate_names = auth.info.name.split(' ')
+            user.first_name = separate_names[0]
+            user.last_name = separate_names[1..-1].join(' ')
+
             user.email = auth.info.email
             user.password = Devise.friendly_token[0, 20]
 
