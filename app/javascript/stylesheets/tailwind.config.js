0d06be7f9a8ab626ab2e8540dd4aacb356dad102
Bruncky <brunobrunckhorst@gmail.com>
2020-06-13 13:01:35 +0200

    Moved tailwind.config to correct location

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,15 +1,12 @@
 module.exports = {
     purge: [],
     theme: {
-        font-family: {
+        fontFamily: {
             display: ['Montserrat', 'sans-serif'],
             body: ['Montserrat', 'sans-serif']
         },
         colors: {
             seeql: '#5BC0BE'
         }
-        extend: {},
-    },
-    variants: {},
-    plugins: [],
+    }
 }
