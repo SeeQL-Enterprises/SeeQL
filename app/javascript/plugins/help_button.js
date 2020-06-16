506e21febfc457dbd8d1b90a1b28cb195aca00ba
Bruncky <brunobrunckhorst@gmail.com>
2020-06-16 22:18:32 +0200

    Fixed JS so we don't have to press button twice for it to work

--- app/javascript/plugins/help_button.js
+++ app/javascript/plugins/help_button.js
@@ -4,10 +4,16 @@ if (document.getElementById("help-button")) {
     helpButton.addEventListener("click", () => {
         const helpNestedList = document.getElementById("help-nested-list")
 
-        if (helpNestedList.style.maxHeight === "0px") {
+        if (helpNestedList.style.maxHeight !== "102px") {
+
             helpNestedList.style.maxHeight = "102px";
+            helpButton.blur();
+
         } else {
+
             helpNestedList.style.maxHeight = "0px";
+            helpButton.blur();
+
         }
     });
 }
