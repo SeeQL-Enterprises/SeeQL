3dda2219b93cf5afc6c6b10c7a1a0cb5f049a5a3
Bruncky <brunobrunckhorst@gmail.com>
2020-06-16 12:27:16 +0200

    Added new SessionsController to Devise override

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
     resources :databases, only: [:show]
 
-    devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+    devise_for :users, controllers: {
+        sessions: 'users/sessions',
+        omniauth_callbacks: 'users/omniauth_callbacks'
+    }
 
     resources :projects, only: [:new, :create, :show, :index] do
         resources :collaborators, only: [:new, :create]
