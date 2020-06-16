96500bbd3291adddfdced0b33a053376191d05f8
Bruncky <brunobrunckhorst@gmail.com>
2020-06-17 01:09:44 +0200

    Added route for registrations error

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
     devise_for :users, controllers: {
         sessions: 'users/sessions',
+        registrations: 'users/registrations',
         omniauth_callbacks: 'users/omniauth_callbacks'
     }
 
