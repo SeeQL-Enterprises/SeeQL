794d7e1359e11accbb93ad71983a2975b3d2a25f
Bruncky <brunobrunckhorst@gmail.com>
2020-06-17 13:57:32 +0200

    Added missing validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,10 @@ class User < ApplicationRecord
     has_many :projects
     has_many :comments, as: :commentable
 
+    validates :first_name, presence: true
+    validates :last_name, presence: true
+    validates :email, presence: true, uniqueness: true
+
     has_one_attached :avatar
 
     def self.from_omniauth(auth)
