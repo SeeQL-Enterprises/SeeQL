8ef4f57fd971d5010aee0104dbcf0bb4ab447c2d
Bruncky <brunobrunckhorst@gmail.com>
2020-06-17 14:29:20 +0200

    Renamed my buttons to more clearly define them

--- app/javascript/plugins/error_lists.js
+++ app/javascript/plugins/error_lists.js
@@ -1,7 +1,7 @@
-var signupButton = document.querySelector('#signup-button');
+var signupButton = document.querySelector('#signup-button-sidebar');
 var signupErrors = document.querySelector('.signupErrorsList');
 
-var loginButton = document.querySelector('#login-button');
+var loginButton = document.querySelector('#login-button-sidebar');
 var loginErrors = document.querySelector('.loginErrors');
 
 signupButton.addEventListener("click", () => {
