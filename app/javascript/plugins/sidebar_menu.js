aa3d7d4d509c361110454b72860a1d96220e3b1e
Bruncky <brunobrunckhorst@gmail.com>
2020-06-17 15:58:49 +0200

    Extended sidebar JS to close menu when user submits form

--- app/javascript/plugins/sidebar_menu.js
+++ app/javascript/plugins/sidebar_menu.js
@@ -1,7 +1,18 @@
-let menu = document.querySelector( "#my-sidenav" );
-let menuIcon = document.querySelector( "#my-burger-menu" );
-let bodyBackground = document.querySelector( "#body" );
+const menu = document.querySelector( "#my-sidenav" );
+const menuIcon = document.querySelector( "#my-burger-menu" );
+const bodyBackground = document.querySelector( "#body" );
 
-menuIcon.addEventListener( "click",()=> {
-  menu.classList.contains('open') ? menu.classList.remove('open') : menu.classList.add('open');
+const signupButtonForm = document.querySelector('#signup-button-form');
+const loginButtonForm = document.querySelector('#login-button-form');
+
+menuIcon.addEventListener( "click", () => {
+    menu.classList.contains('open') ? menu.classList.remove('open') : menu.classList.add('open');
+});
+
+signupButtonForm.addEventListener("click", () => {
+    menu.classList.remove('open');
+});
+
+loginButtonForm.addEventListener("click", () => {
+    menu.classList.remove('open');
 });
