b068f02df3cfd6f74c6ddcc4b35ca3933c94e1fe
Bruncky <brunobrunckhorst@gmail.com>
2020-06-17 22:27:58 +0200

    Adapted validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,9 @@ class User < ApplicationRecord
     has_many :projects
     has_many :comments, as: :commentable
 
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    validates :email, presence: true, uniqueness: true
+    validates :first_name, presence: true, on: :create
+    validates :last_name, presence: true, on: :create
+    validates :email, presence: true, uniqueness: true, on: :create
 
     has_one_attached :avatar
 
