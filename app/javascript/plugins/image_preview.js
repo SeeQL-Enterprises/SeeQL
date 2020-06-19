998a310ada2a4b56319ed3078b963790778f4393
Bruncky <brunobrunckhorst@gmail.com>
2020-06-19 21:33:08 +0200

    Added functionality to change the text on the upload button to filename

--- app/javascript/plugins/image_preview.js
+++ app/javascript/plugins/image_preview.js
@@ -1,34 +1,48 @@
 if (document.getElementById("user_avatar")) {
 
-    var userAvatar = document.getElementById("user_avatar")
+    var userAvatar = document.getElementById("user_avatar");
 
     userAvatar.onchange = function () {
+
         var reader = new FileReader();
 
         reader.onload = function (event) {
+
             // Get loaded data and render a thumbnail
             document.getElementById("avatar-preview-image").src = event.target.result;
+
         };
 
         // Read the image file as a data URL
         reader.readAsDataURL(this.files[0]);
 
-    }
-} else if (document.getElementById("project_project_image")) {
+    };
 
-    var projectImage = document.getElementById("project_project_image")
+} else if (document.getElementById("project_image")) {
 
-    projectImage.onchange = function () {
+    var projectImage = document.getElementById("project_image");
+
+    projectImage.onchange = function (event) {
+
+        // Define the image preview, the button below it,
+        // the file that was uploaded and a new FileReader instance
+        var previewImage = document.getElementById("project-preview-image");
+        var previewImageButton = document.getElementById("project-image-button");
+        var file = event.srcElement.files[0];
         var reader = new FileReader();
 
         reader.onload = function (event) {
-            // Get loaded data and render a thumbnail
-            document.getElementById("project-preview-image").src = event.target.result;
-            document.getElementById("project-preview-image").style = "object-fit: cover";
+
+            // Get loaded data and render a thumbnail,
+            // then set the button's text to the name of the file
+            previewImage.src = event.target.result;
+            previewImageButton.innerHTML = file.name;
+            previewImage.style = "object-fit: cover";
+
         };
 
         // Read the image file as a data URL
         reader.readAsDataURL(this.files[0]);
+    };
 
-    }
 }
