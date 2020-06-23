49122c5a2d5a36709690f2af699aac06a6ee74a2
Bruncky <brunobrunckhorst@gmail.com>
2020-06-23 23:01:46 +0200

    Implemented basic fade effect for sidebar text (needs to be fixed)

--- app/javascript/plugins/sidebar.js
+++ app/javascript/plugins/sidebar.js
@@ -1,16 +1,37 @@
 const sidebar = document.getElementById( "sidebar" );
 const sidebarIcon = document.getElementById( "sidebar-opener" );
+const sidebarText = document.getElementById("sidebar-text");
 const signupButtonForm = document.querySelector('#signup-button-form');
 const loginButtonForm = document.querySelector('#login-button-form');
 
 sidebarIcon.addEventListener( "click", () => {
-    sidebar.classList.contains('open') ? sidebar.classList.remove('open') : sidebar.classList.add('open');
+
+    if (sidebar.classList.contains('open')) {
+
+        $(sidebarText).addClass("hidden").fadeOut(200);
+
+        setTimeout(() => {
+            sidebar.classList.remove('open');
+        }, 200);
+
+    } else {
+
+        sidebar.classList.add('open');
+        $(sidebarText).removeClass('hidden').fadeIn(200);
+
+    }
+
 });
 
+// Make sure that submitting login/signup forms will close the menu
 signupButtonForm.addEventListener("click", () => {
+
     sidebar.classList.remove('open');
+
 });
 
 loginButtonForm.addEventListener("click", () => {
+
     sidebar.classList.remove('open');
+
 });
