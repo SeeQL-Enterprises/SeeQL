3345833d6e482dfd4db5cd2e8d2c32cb705ca3b1
Bruncky <brunobrunckhorst@gmail.com>
2020-06-29 21:59:20 +0200

    Added fading animation to show dropdown on click

--- app/javascript/plugins/project_options.js
+++ app/javascript/plugins/project_options.js
@@ -1,12 +1,17 @@
 if (document.getElementsByClassName('project-options-icon')) {
 
     const projectOptionsIcons = document.getElementsByClassName('project-options-icon');
+    const projectOptionsDropdown = document.getElementById('card-project-options-dropdown');
 
     for(let i = 0; i < projectOptionsIcons.length; i++) {
 
         projectOptionsIcons[i].addEventListener("click", () => {
 
-            console.log("test");
+            $(projectOptionsDropdown).fadeToggle(400);
+
+            setTimeout(() => {
+                $(projectOptionsDropdown).fadeOut(500)
+            }, 4000);
 
         });
     }
