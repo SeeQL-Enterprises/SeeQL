c974f33cfc91d2e53962210e53600033563ccb1d
Bruno A. Brunckhorst <brunobrunckhorst@gmail.com>
2021-03-05 14:45:20 +0100

    Cleanup for controller tests

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class ProjectsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+    # test "the truth" do
+    #   assert true
+    # end
 end
